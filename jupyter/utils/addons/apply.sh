bb2a8025b49c2c2930d4b616d0ce04aaa6096e3e
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2025-03-25 13:38:27 +0000

    Test loading iframe

--- jupyter/utils/addons/apply.sh
+++ jupyter/utils/addons/apply.sh
@@ -11,16 +11,10 @@ pf_url="https://unpkg.com/@patternfly/patternfly@6.0.0/patternfly.min.css"
 static_dir="/opt/app-root/share/jupyter/lab/static"
 index_file="$static_dir/index.html"
 
-head_file="$script_dir/partial-head.html"
 body_file="$script_dir/partial-body.html"
 
 if [ ! -f "$index_file" ]; then
   echo "File '$index_file' not found"
-  exit 1
-fi
-
-if [ ! -f "$head_file" ]; then
-  echo "File '$head_file' not found"
   exit 1
 fi
 
@@ -31,10 +25,13 @@ fi
 
 curl -o "$static_dir/pf.css" "$pf_url"
 
-head_content=$(tr -d '\n' <"$head_file" | sed 's/@/\\@/g')
 body_content=$(tr -d '\n' <"$body_file" | sed 's/@/\\@/g')
 
-perl -i -0pe "s|</head>|$head_content\n</head>|" "$index_file"
-perl -i -0pe "s|</body>|$body_content\n</body>|" "$index_file"
+perl -i -0pe "s|</body>|$body_content\n</body>|g" "$index_file"
+
+if [ $? -ne 0 ]; then
+  echo "Error: Perl substitution failed"
+  exit 1
+fi
 
 echo "Content from partial HTML files successfully injected into JupyterLab's 'index.html' file"
